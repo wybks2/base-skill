13c9332ab6b370deb11bba15ebfa9b57504f2616
wybks2 <1105196302@qq.com>
2021-09-30 00:38:18 +0800

    发布订阅模式 和 观察者模式

--- js/try/eventEmit.js
+++ js/try/eventEmit.js
@@ -1,1 +1,18 @@
-// 发布订阅模式
+// 发布订阅模式  发布者   
+class EventEmitter{
+    constructor() {
+        // 中间调度
+        this.events = {
+
+        }
+    }
+    // 发布
+    on(type, cb) {
+       if(!this.events[type]) {
+           this.events[type] = [cb]
+       }else{
+           this.events[type].push(cb)
+       }
+    }
+
+}
