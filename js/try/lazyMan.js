28ccaf72a8562c08d7b1ce407030ed33ee70a620
wybks2 <1105196302@qq.com>
2021-10-09 11:40:30 +0800

    暂时提交 设计模式

--- js/try/lazyMan.js
+++ js/try/lazyMan.js
@@ -1,36 +1,21 @@
 
 
 class Lazy {
-    constructor(x) {
-        console.log(x)
-        this.tasks = [];
-        this.time = 0;
-    }
-    async next() {
-        return new Promise((reslove) => {
-            this.tasks.shift()(this.time, reslove);
-        })
-    }
-    eat(name) {
-        console.log(`eat${name}`)
-        this.tasks.push()
-        return this
-    }
-    async sleep(delay) {
-        this.time = delay;
-        console.log(this.time)
-        this.tasks.push(this.delay);
-        await this.next();
-        console.log("立马")
-        return this
-    }
-    delay(delay, cb) {
-        console.log(delay)
-        setTimeout(() => {
-            console.log("walk", delay)
-            cb();
-        }, delay)
-    }
+  constructor(name) {
+      // 创建一个任务队列
+      this.tasks = [];
+      const task = () => {
+          console.log(`Hi! this is ${name}`)
+          this.next();
+      }
+      tasks.push(task);
+      setTimeout(() => {
+        this.next();
+      }, 0)
+  }
+  eat(some) {
+    
+  }
 }
 
 
---
+++ others/dsl.md
@@ -0,0 +1,3 @@
+1. 什么是dsl
+> domain specified language 领域专用语言
+> react的jsx vue的template
