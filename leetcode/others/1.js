55a9f36eda42abb40c7ab4809f86602706951431
wybks2 <1105196302@qq.com>
2021-10-19 17:55:39 +0800

    两数之和 map解法

--- leetcode/others/1.js
+++ leetcode/others/1.js
@@ -15,5 +15,27 @@
 
 var twoSum2 = function(nums, target) {
     // 
-    const map = {};
+    for(let i = 0; i<nums.length; i++) {
+        for(let j = 0; j<nums.length; j++) {
+            if(i == j) {
+                return;
+            }
+            if(nums[i] + nums[j] == target) {
+                return [i, j]
+            }
+        }
+    }
+    return false;
+ };
+
+ var twoSum3 = function(nums, target) {
+    // 
+    const sumMap = new Map();
+    for(let i = 0; i< nums.length; i++) {
+        if(sumMap.has(nums[i])) {
+            sumMap.set(target-nums[i], i)
+        } else {
+            return [sumMap.get(nums[i]), j]
+        }
+    }
  };
